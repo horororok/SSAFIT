a63ba6f923e043801261b768a0c603ede394fffd
Lee jiwoo <jwl010362@gmail.com>
2023-11-16 00:21:04 +0900

    user 컨트롤러 로그인 수정(세션)

--- BACK/src/main/java/com/ssafy/ssafit/controller/UserController.java
+++ BACK/src/main/java/com/ssafy/ssafit/controller/UserController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,16 +63,18 @@ public class UserController {
 		
 		//로그인 실패
 		if(temp == null)
-			return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 		//로그인 성공
-		session.setAttribute("loginUser", temp.getName());
-		return new ResponseEntity<String>(temp.getName(), HttpStatus.OK);
+		session.setAttribute("loginUser", temp);
+		return new ResponseEntity<User>(temp, HttpStatus.OK);
 	}
 	
 	@GetMapping("logout")
 	public ResponseEntity<Void> logout(HttpSession session){
 		session.invalidate();
 		
+//		if(session != null)
+//			return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 
