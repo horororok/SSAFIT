275ab3e7206a1f33494f6d084ad7088b549fa9c5
jiwoo <jwl0103@naver.com>
2023-11-20 17:51:01 +0900

    찜한 영상 목록 반환

--- vue-api-project/src/components/mypage/MyPageZZIM.vue
+++ vue-api-project/src/components/mypage/MyPageZZIM.vue
@@ -1,35 +1,58 @@
 <template>
     <div>
         <div>좋아요 누른 영상 리스트</div>
-        <ul>
-            <li v-for="likedVideo in likedVideos" :key="likedVideo.id">
-                <router-link :to="{ name: 'detail', params: { videoId: likedVideo.id } }">
-                    {{ likedVideo.title }}
-                </router-link>
-            </li>
-        </ul>
+        <!-- 카드 형식으로 비디오 목록을 표시 -->
+    <div class="row">
+      <div class="col-md-4" v-for="likedVideo in videoStore.likedVideos" :key="likedVideo.id">
+        <div class="card mb-4">
+          <img :src="likedVideo.thumbnail" class="card-img-top" alt="Thumbnail">
+          <div class="card-body">
+            <h5 class="card-title clickable-text" @click="showVideoDetail(likedVideo.video_id)">
+              <!-- 제목이 두 줄 이상이면 생략 처리 -->
+              <span style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">
+                {{ likedVideo.title }}
+              </span>
+            </h5>
+            <p class="card-text"><strong>{{ likedVideo.channel_name }}</strong></p>
+            <p class="card-text"><strong>파트</strong> {{ likedVideo.part }} | <strong>조회수</strong> {{ likedVideo.view_cnt }}</p>
+            <!-- 좋아요 개수 표시 -->
+            <div class="d-flex align-items-center">
+              <span>{{ likedVideo.like_cnt }}</span>
+              <span >
+                ❤️
+              </span>
+              <!-- 하트 클릭하면 unlike 실행하도록 수정!! -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue'
-import {useVideoStore} from '@/stores/video'
-import {useUserStore} from '@/stores/user'
+import {ref, onMounted} from 'vue';
+import {useVideoStore} from '@/stores/video';
+import {useUserStore} from '@/stores/user';
+import router from "@/router";
 
 const userStore = useUserStore()
 const videoStore = useVideoStore()
 
-const likedVideos = ref([])
-
-const getLikedVideos = ()=>{
-    const userId = userStore.loginUserObj.user_id;
-    const response = videoStore.getLikedVideos(userId)
-    likedVideos.value = response.data
-}
-
 onMounted (()=>{
-    getLikedVideos()
+    const userId = userStore.loginUserObj.user_id;
+    videoStore.getLikedVideos(userId);
 })
+
+// 비디오 상세 페이지로 이동
+const showVideoDetail = function (videoId) {
+  if (videoId) {
+    // videoId가 유효하면 비디오 상세 페이지로 이동
+    router.push({ name: 'videoDetail', params: { videoId: videoId } });
+  } else {
+    console.error("Invalid videoId:", videoId);
+  }
+}
 
 
 </script>
