99e8de1e5d345fb2fcd9df9407e299ddedbc1445
Lee jiwoo <jwl010362@gmail.com>
2023-11-21 01:52:19 +0900

    message service 작성

--- BACK/src/main/java/com/ssafy/ssafit/model/service/MessageServiceImpl.java
+++ BACK/src/main/java/com/ssafy/ssafit/model/service/MessageServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ssafy.ssafit.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.ssafit.model.dao.MessageDao;
+import com.ssafy.ssafit.model.dto.Message;
 
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -13,6 +16,42 @@ public class MessageServiceImpl implements MessageService {
 	@Autowired
 	public MessageServiceImpl(MessageDao messageDao) {
 		this.messageDao = messageDao;
+	}
+
+	@Override
+	public int writeMessage(Message message) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Message readMessage(int message_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getUnreadMessage(int user_to_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getReceivedMessage(int user_to_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getSentMessage(int user_from_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getAllMessage(int userId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
